212634be0d960e016c705a095aa703b533e18e62
isjahputera <57501190+isjahputera@users.noreply.github.com>
2019-11-29 20:47:35 -0700

    Refactoring PlayZork() in zork.py. In progress.

--- src/zork.py
+++ src/zork.py
@@ -207,3 +207,167 @@ def Play_Zork():
 				exit()
 			if exit_inp.lower() == ("y"):
 				Play_Zork()
+
+def Play_Zork():
+	loop = 0
+	while True:
+		if loop == 0:
+			loop = 4
+			print("---------------------------------------------------------")
+			print("Welcome to Zork - The Unofficial Python Version.")
+
+		if loop == 4:
+			loop = south()
+
+		if loop == 1:
+			loop = north()
+
+		if loop == 8:
+			loop = southwest()
+
+		if loop == -1:
+			break
+	exit()
+
+def south():
+	# First Input Loop
+	return_value = 0
+	while True:
+		print("---------------------------------------------------------")
+		print("You are standing in an open field west of a white house, with a boarded front door.")
+		print("You can see a small lake to the north.")
+		print("(A secret path leads southwest into the forest.)")
+		print("There is a Small Mailbox.")
+		second = input("What do you do? ")
+
+		if second.lower() == ("take mailbox"):
+			print("---------------------------------------------------------")
+			print("It is securely anchored.")
+		elif second.lower() == ("open mailbox"):
+			print("---------------------------------------------------------")
+			print("Opening the small mailbox reveals a leaflet.")
+		elif second.lower() == ("go north"):
+			return_value = 1
+			break
+		elif second.lower() == ("open door"):
+			print("---------------------------------------------------------")
+			print("The door cannot be opened.")
+		elif second.lower() == ("take boards"):
+			print("---------------------------------------------------------")
+			print("The boards are securely fastened.")
+		elif second.lower() == ("look at house"):
+			print("---------------------------------------------------------")
+			print(
+				"The house is a beautiful colonial house which is painted white. It is clear that the owners must have been extremely wealthy.")
+		elif second.lower() == ("go southwest"):
+			return_value = 8
+			break
+		elif second.lower() == ("read leaflet"):
+			print("---------------------------------------------------------")
+			print("Welcome to the Unofficial Python Version of Zork. Your mission is to find a Jade Statue.")
+		elif second.lower() == ("kick the bucket"):
+			print("---------------------------------------------------------")
+			print("You die.")
+			print("---------------------------------------------------------")
+			dead_inp = input("Do you want to continue? Y/N ")
+			if dead_inp.lower() == ("n"):
+				return_value = -1
+			if dead_inp.lower() == ("y"):
+				return_value = 0
+			break
+		else:
+			print("---------------------------------------------------------")
+	return return_value
+
+def north():
+	# North of House
+	return_value = 0
+	while True:
+		print("---------------------------------------------------------")
+		print("You find yourself at the edge of a beautiful lake aside rolling hills.")
+		print("A small pier juts out into the lake.")
+		print("A fishing rod rests on the pier.")
+		print("(You can see a white house in the distance to the south.)")
+		north_house_inp = input("What do you do? ")
+
+		if north_house_inp.lower() == ("go south"):
+			return_value = 4
+			break
+		elif north_house_inp.lower() == ("swim"):
+			print("---------------------------------------------------------")
+			print("You don't have a change of clothes and you aren't here on vacation.")
+		elif north_house_inp.lower() == ("fish"):
+			print("---------------------------------------------------------")
+			print("You spend some time fishing but nothing seems to bite.")
+		elif north_house_inp.lower() == ("kick the bucket"):
+			print("---------------------------------------------------------")
+			print("You die.")
+			print("---------------------------------------------------------")
+			dead_inp = input("Do you want to continue? Y/N ")
+			if dead_inp.lower() == ("n"):
+				return_value = -1
+			if dead_inp.lower() == ("y"):
+				return_value = 0
+			break
+		else:
+			print("---------------------------------------------------------")
+	return return_value
+
+def southwest():
+	return_value = 0
+	while True:
+		print("---------------------------------------------------------")
+		print("This is a forest, with trees in all directions. To the east, there appears to be sunlight.")
+		forest_inp = input("What do you do? ")
+
+		if forest_inp.lower() == ("go west"):
+			print("---------------------------------------------------------")
+			print("You would need a machete to go further west.")
+		elif forest_inp.lower() == ("go north"):
+			print("---------------------------------------------------------")
+			print("The forest becomes impenetrable to the North.")
+		elif forest_inp.lower() == ("go south"):
+			print("---------------------------------------------------------")
+			print("Storm-tossed trees block your way.")
+		elif forest_inp.lower() == ("go east"):
+			return_value = 9
+			break
+		elif forest_inp.lower() == ("kick the bucket"):
+			print("---------------------------------------------------------")
+			print("You die.")
+			print("---------------------------------------------------------")
+			dead_inp = input("Do you want to continue? Y/N ")
+			if dead_inp.lower() == ("n"):
+				return_value = -1
+			if dead_inp.lower() == ("y"):
+				return_value = 0
+			break
+		else:
+			print("---------------------------------------------------------")
+
+def east():
+	# East Loop and Grating Input
+	
+	while loop == 9:
+		if loop == 9:
+			print("---------------------------------------------------------")
+			print("You are in a clearing, with a forest surrounding you on all sides. A path leads south.")
+			print("There is an open grating, descending into darkness.")
+			grating_inp = input("What do you do? ")
+
+		if grating_inp.lower() == ("go south"):
+			print("---------------------------------------------------------")
+			print("You see a large ogre and turn around.")
+		elif grating_inp.lower() == ("descend grating"):
+			loop = 10
+		elif grating_inp.lower() == ("kick the bucket"):
+			print("---------------------------------------------------------")
+			print("You die.")
+			print("---------------------------------------------------------")
+			dead_inp = input("Do you want to continue? Y/N ")
+			if dead_inp.lower() == ("n"):
+				exit()
+			if dead_inp.lower() == ("y"):
+				Play_Zork()
+		else:
+			print("---------------------------------------------------------")
